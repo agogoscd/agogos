fb1fe719447a23bc36150d6d40cb29993ddee6c9
Marek Goldmann <mgoldman@redhat.com>
2021-02-12 14:58:37 +0100

    docs: examples moved to other repository, docs updates

--- operator/src/test/resources/samples/apply.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-find $(dirname "$0") -name '*.yaml'  | xargs -n1 kubectl apply -f
-# -not -name "*[generated].yaml"
-find $(dirname "$0") -name '*generated.yaml' | xargs -n1 kubectl create -f
