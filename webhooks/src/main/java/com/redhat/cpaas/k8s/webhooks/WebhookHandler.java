ee7ea4bc485eb8237b4fdee522f9472d225002fa
Marek Goldmann <mgoldman@redhat.com>
2021-02-16 11:48:09 +0100

    feat(webhooks): by default process every resource we know
    Allow all non-handled requests by default.

--- webhooks/src/main/java/com/redhat/cpaas/k8s/webhooks/WebhookHandler.java
+++ webhooks/src/main/java/com/redhat/cpaas/k8s/webhooks/WebhookHandler.java
@@ -9,9 +9,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.redhat.cpaas.k8s.webhooks.mutator.ComponentBuildMutator;
+import com.redhat.cpaas.k8s.webhooks.validator.ComponentBuildValidator;
 import com.redhat.cpaas.k8s.webhooks.validator.ComponentValidator;
+import com.redhat.cpaas.v1alpha1.ComponentBuildResource;
+import com.redhat.cpaas.v1alpha1.ComponentResource;
 
+import io.fabric8.kubernetes.api.model.KubernetesResource;
+import io.fabric8.kubernetes.api.model.admission.AdmissionResponseBuilder;
 import io.fabric8.kubernetes.api.model.admission.AdmissionReview;
+import io.fabric8.kubernetes.api.model.admission.AdmissionReviewBuilder;
 
 @Path("/webhooks")
 @Produces(MediaType.APPLICATION_JSON)
@@ -23,17 +29,48 @@ public class WebhookHandler {
     ComponentValidator componentValidator;
 
     @Inject
+    ComponentBuildValidator componentBuildValidator;
+
+    @Inject
     ComponentBuildMutator componentBuildMutator;
 
     @POST
-    @Path("validate/component")
-    public AdmissionReview validateComponent(AdmissionReview admissionReview) {
-        return componentValidator.validate(admissionReview);
+    @Path("validate")
+    public AdmissionReview validate(AdmissionReview admissionReview) {
+        KubernetesResource resource = admissionReview.getRequest().getObject();
+
+        if (resource instanceof ComponentResource) {
+            return componentValidator.validate(admissionReview);
+        }
+
+        if (resource instanceof ComponentBuildResource) {
+            return componentBuildValidator.validate(admissionReview);
+        }
+
+        // By default allow requests
+        return allow(admissionReview);
     }
 
     @POST
-    @Path("mutate/componentbuild")
+    @Path("mutate")
     public AdmissionReview mutateComponentBuild(AdmissionReview admissionReview) {
-        return componentBuildMutator.mutate(admissionReview);
+        KubernetesResource resource = admissionReview.getRequest().getObject();
+
+        if (resource instanceof ComponentBuildResource) {
+            return componentBuildMutator.mutate(admissionReview);
+        }
+
+        // By default allow requests
+        return allow(admissionReview);
+    }
+
+    private AdmissionReview allow(AdmissionReview admissionReview) {
+        AdmissionResponseBuilder responseBuilder = new AdmissionResponseBuilder() //
+                .withUid(admissionReview.getRequest().getUid()).withAllowed(true);
+
+        AdmissionReviewBuilder reviewBuilder = new AdmissionReviewBuilder() //
+                .withApiVersion(admissionReview.getApiVersion());
+
+        return reviewBuilder.withResponse(responseBuilder.build()).build();
     }
 }
---
+++ webhooks/src/main/java/com/redhat/cpaas/k8s/webhooks/validator/ComponentBuildValidator.java
@@ -0,0 +1,55 @@
+package com.redhat.cpaas.k8s.webhooks.validator;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import com.redhat.cpaas.errors.ApplicationException;
+import com.redhat.cpaas.errors.MissingResourceException;
+import com.redhat.cpaas.k8s.client.ComponentResourceClient;
+import com.redhat.cpaas.v1alpha1.ComponentBuildResource;
+import com.redhat.cpaas.v1alpha1.ComponentResource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.fabric8.kubernetes.api.model.StatusBuilder;
+import io.fabric8.kubernetes.api.model.admission.AdmissionResponseBuilder;
+
+@ApplicationScoped
+public class ComponentBuildValidator extends Validator<ComponentBuildResource> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ComponentBuildValidator.class);
+
+    @Inject
+    ComponentResourceClient componentResourceClient;
+
+    @Override
+    protected void validateResource(ComponentBuildResource componentBuild, AdmissionResponseBuilder responseBuilder) {
+        try {
+            validateComponentBuild(componentBuild);
+
+            responseBuilder.withAllowed(true);
+        } catch (ApplicationException e) {
+            LOG.error("An error occurred while validating Component Build", e);
+
+            responseBuilder.withAllowed(false) //
+                    .withStatus(new StatusBuilder() //
+                            .withCode(400) //
+                            .withMessage(e.getMessage()) //
+                            .build());
+        }
+    }
+
+    private void validateComponentBuild(ComponentBuildResource componentBuild) throws ApplicationException {
+        LOG.info("Validating component build '{}'", componentBuild.getNamespacedName());
+
+        ComponentResource component = componentResourceClient.getByName(componentBuild.getSpec().getComponent());
+
+        if (component == null) {
+            throw new MissingResourceException("Selected Component '{}' does not exist in '{}' namespace",
+                    componentBuild.getSpec().getComponent(), componentBuild.getMetadata().getNamespace());
+        }
+
+        LOG.info("Component build '{}' is valid!", componentBuild.getNamespacedName());
+    }
+}
