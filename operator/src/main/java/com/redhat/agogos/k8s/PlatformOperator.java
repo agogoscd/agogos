863a633115fe3af8703a36c65a7d5da436215632
Greg Allen <gallen@redhat.com>
2023-03-31 20:53:31 +0000

    Revert "Merge branch 'workaround-mapper-issue' into 'main'"
    This reverts merge request !65

--- operator/src/main/java/com/redhat/agogos/k8s/PlatformOperator.java
+++ operator/src/main/java/com/redhat/agogos/k8s/PlatformOperator.java
@@ -2,6 +2,7 @@ package com.redhat.agogos.k8s;
 
 import com.redhat.agogos.k8s.controllers.ComponentController;
 import io.javaoperatorsdk.operator.Operator;
+import io.javaoperatorsdk.operator.api.config.ConfigurationService;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import org.jboss.logging.Logger;
@@ -14,11 +15,8 @@ import javax.inject.Inject;
 public class PlatformOperator {
     private static final Logger LOG = Logger.getLogger(PlatformOperator.class);
 
-    // we inject a specific configuration service instance that provides a plain ObjectMapper
-    // instead of using the one from Fabric8 to work around
-    // https://github.com/fabric8io/kubernetes-client/issues/5009
     @Inject
-    AgogosConfigurationServiceProvider.AgogosConfigurationService configuration;
+    ConfigurationService configuration;
 
     /**
      * Required to start the operator and register all controllers automatically.
