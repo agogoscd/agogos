30c0331a140a7a509df6fa677286ed0c0d507866
Greg Allen <gallen@redhat.com>
2023-09-14 16:34:23 -0400

    feat(AGOGOS-324): disable resource deletion for development profile

--- operator/src/main/java/com/redhat/agogos/operator/k8s/controllers/build/PipelineRunPrecondition.java
+++ operator/src/main/java/com/redhat/agogos/operator/k8s/controllers/build/PipelineRunPrecondition.java
@@ -8,16 +8,24 @@ import io.fabric8.tekton.pipeline.v1beta1.PipelineRun;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.workflow.Condition;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+import java.util.Optional;
 
 public class PipelineRunPrecondition implements Condition<PipelineRun, Build> {
+
+    Optional<Boolean> retainResources = ConfigProvider.getConfig().getOptionalValue("agogos.retain-resources", Boolean.class);
+
     @Override
     public boolean isMet(DependentResource<PipelineRun, Build> dependentResource, Build build, Context<Build> context) {
         // Reconcile under the following conditions:
         //   1. Create a PipelineRun resource only when the Component has a status of Ready.
-        //   2. The dependent PipelineRun resource is deleted when the Build status is Finished.
+        //   2. The dependent PipelineRun resource is deleted when the Build status is Finished (unless retainResources
+        //      is set to true).
         //   3. A new PipelineRun is not created when the Build status is Finished.
         var component = context.getSecondaryResource(Component.class);
-        return component.map(c -> c.getStatus().getStatus() == ResourceStatus.READY).orElse(false)
-                && build.getStatus().getStatus() != ResultableResourceStatus.FINISHED;
+        return retainResources.orElse(false) ||
+                (component.map(c -> c.getStatus().getStatus() == ResourceStatus.READY).orElse(false) &&
+                        build.getStatus().getStatus() != ResultableResourceStatus.FINISHED);
     }
 }
--- operator/src/main/java/com/redhat/agogos/operator/k8s/controllers/run/PipelineRunPrecondition.java
+++ operator/src/main/java/com/redhat/agogos/operator/k8s/controllers/run/PipelineRunPrecondition.java
@@ -8,16 +8,24 @@ import io.fabric8.tekton.pipeline.v1beta1.PipelineRun;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.workflow.Condition;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+import java.util.Optional;
 
 public class PipelineRunPrecondition implements Condition<PipelineRun, Run> {
+
+    Optional<Boolean> retainResources = ConfigProvider.getConfig().getOptionalValue("agogos.retain-resources", Boolean.class);
+
     @Override
     public boolean isMet(DependentResource<PipelineRun, Run> dependentResource, Run run, Context<Run> context) {
         // Reconcile under the following conditions:
         //   1. Create a PipelineRun resource only when the Pipeline has a status of Ready.
-        //   2. The dependent PipelineRun resource is deleted when the Run status is Finished.
+        //   2. The dependent PipelineRun resource is deleted when the Run status is Finished (unless retainResources
+        //      is set to true).
         //   3. A new PipelineRun is not created when the Run status is Finished.
         var pipeline = context.getSecondaryResource(Pipeline.class);
-        return pipeline.map(c -> c.getStatus().getStatus() == ResourceStatus.READY).orElse(false)
-                && run.getStatus().getStatus() != ResultableResourceStatus.FINISHED;
+        return retainResources.orElse(false) ||
+                (pipeline.map(c -> c.getStatus().getStatus() == ResourceStatus.READY).orElse(false) &&
+                        run.getStatus().getStatus() != ResultableResourceStatus.FINISHED);
     }
 }
