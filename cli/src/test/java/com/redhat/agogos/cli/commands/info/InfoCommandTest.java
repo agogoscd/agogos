493e727959b64dfdee0a91a5e09694477c2153e8
Greg Allen <gallen@redhat.com>
2023-09-29 16:04:12 -0400

    test(AGOGOS-137): add tests for the info command

--- cli/src/test/java/com/redhat/agogos/cli/commands/info/InfoCommandTest.java
+++ cli/src/test/java/com/redhat/agogos/cli/commands/info/InfoCommandTest.java
@@ -1,48 +1,39 @@
 package com.redhat.agogos.cli.commands.info;
 
 import com.redhat.agogos.cli.commands.AbstractCommandTest;
-import com.redhat.agogos.core.KubernetesFacade;
 import io.fabric8.kubernetes.client.VersionInfo;
-import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
-import picocli.CommandLine;
+import picocli.CommandLine.ExitCode;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.net.URL;
 
 @QuarkusTest
 public class InfoCommandTest extends AbstractCommandTest {
 
-    @InjectMock
-    KubernetesFacade kubernetesFacadeMock;
+    @Test
+    public void infoHelp() throws Exception {
+        int returnCode = cli.run(catcher.getOut(), catcher.getErr(), "info", "--help");
 
-    @InjectMocks
-    InfoCommand infoCommand = new InfoCommand();
+        Assertions.assertEquals(ExitCode.OK, returnCode);
+        Assertions.assertTrue(catcher.compareToStdout(utils.testResourceAsStringList("info/help.txt")));
+    }
 
     @Test
-    public void ensureDataIsPrinted() throws Exception {
-        StringWriter sw = new StringWriter();
-
+    public void infoOutput() throws Exception {
         VersionInfo versionInfo = new VersionInfo.Builder().withMajor("x").withMinor("y").build();
-        Mockito.when(kubernetesFacadeMock.getMasterUrl()).thenReturn(new URL("https://localhost:6443"));
-        Mockito.when(kubernetesFacadeMock.getNamespace()).thenReturn("default");
-        Mockito.when(kubernetesFacadeMock.getKubernetesVersion()).thenReturn(versionInfo);
+        Mockito.when(kubernetesFacadeMock.getMasterUrl())
+                .thenReturn(new URL("https://localhost:6443"));
+        Mockito.when(kubernetesFacadeMock.getNamespace())
+                .thenReturn("default");
+        Mockito.when(kubernetesFacadeMock.getKubernetesVersion())
+                .thenReturn(versionInfo);
 
-        CommandLine cmd = new CommandLine(infoCommand);
-        cmd.setOut(new PrintWriter(sw));
+        int returnCode = cli.run(catcher.getOut(), catcher.getErr(), "info");
 
-        int returnCode = cmd.execute();
-
-        Assertions.assertEquals(0, returnCode);
-        String output = sw.toString();
-        Assertions.assertTrue(output.contains("https://localhost:6443"));
-        Assertions.assertTrue(output.contains("default"));
-        Assertions.assertTrue(output.contains("x"));
-        Assertions.assertTrue(output.contains("y"));
+        Assertions.assertEquals(ExitCode.OK, returnCode);
+        Assertions.assertTrue(catcher.compareToStdout(utils.testResourceAsStringList("info/info-output.txt")));
     }
 }
